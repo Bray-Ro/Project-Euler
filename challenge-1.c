4e82aa3a61f7e7bc146b60a6feccc529fe9f9d67
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-10 10:05:30 -0400

    Update challenge-1.c

--- challenge-1.c
+++ challenge-1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int current3 = 3;
 int current5 = 5;
+int total = 0;
 int main(void) {
-  
+
   printf("Project euler 1 By Rohan\n");
   while (current3 < 1000 || current5 < 1000) {
     char current3Str[4];
@@ -10,13 +11,16 @@ int main(void) {
     sprintf(current3Str, "%d", current3);
     sprintf(current5Str, "%d", current5);
     
-    printf("\n%d", current3);
+
     current3 = current3 + 3;
     if (current5 < 1000) {
-    printf("\n%d", current5);
+
     current5 = current5 + 5;
-    }
+    int current5and3 = current3 + current5;
+    total = total + current5and3;
     
+    }
+    printf("\n%d", total);
     
   }
   return 0;
