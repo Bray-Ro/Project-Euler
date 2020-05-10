81b9ae5797bbfb3389508a875d9754307cebe2da
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-10 09:06:58 -0400

    Print all multiples of 5 and 3 up to 1000

---
+++ challenge-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int current3 = 3;
+int current5 = 5;
+int main(void) {
+  
+  printf("Project euler 1 By Rohan\n");
+  while (current3 < 1000 || current5 < 1000) {
+    char current3Str[4];
+    char current5Str[4];
+    sprintf(current3Str, "%d", current3);
+    sprintf(current5Str, "%d", current5);
+    
+    printf("\n%d", current3);
+    printf("\n%d", current5);
+    current3 = current3 + 3;
+    current5 = current5 + 5;
+  }
+  return 0;
+}
